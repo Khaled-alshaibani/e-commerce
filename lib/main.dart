530e8b97ccf5964b5e8632499371754e7bc65e98
Khaled-Al-shaibani <khaled.s.alshaibani@gmail.com>
2026-05-01 19:23:51 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './pages/home_page.dart';
+import './pages/product_list_page.dart';
+import './pages/cart_page.dart';
+import 'providers/product_provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductProvider(),
+      child: MaterialApp(
+        title: 'متجر بسيط',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(secondary: Colors.deepOrange),
+          fontFamily: 'Lato',
+        ),
+        home: const HomePage(),
+        routes: {
+          '/products': (ctx) => const ProductListPage(),
+          '/cart': (ctx) => const CartPage(),
+        },
+      ),
+    );
+  }
+}
